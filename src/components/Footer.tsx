ec7ff50834cbd44d3155ea103e5e12add06bc5e1
ShunKUN1182 <2250378@ecc.ac.jp>
2026-07-15 11:54:13 +0900

    feat: 現在位置に応じてfooterのiconを変更

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,21 +5,27 @@ import iconFriend from "../assets/icons/icon_friend.png";
 import iconFriendActive from "../assets/icons/icon_friend_active.png";
 import iconDM from "../assets/icons/icon_dm.png";
 import iconDMActive from "../assets/icons/icon_dm_active.png";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Footer() {
     return (
         <>
             <footer>
-                <Link to="/">
-                    <img src={iconHome} alt="" />
-                </Link>
-                <Link to="/friend">
-                    <img src={iconFriend} alt="" />
-                </Link>
-                <Link to="/dm">
-                    <img src={iconDM} alt="" />
-                </Link>
+                <NavLink to="/">
+                    {({ isActive }) => (
+                        <img src={isActive ? iconHomeActive : iconHome} alt="HomeIcon" />
+                    )}
+                </NavLink>
+                <NavLink to="/friend">
+                    {({ isActive }) => (
+                        <img src={isActive ? iconFriendActive : iconFriend} alt="HomeIcon" />
+                    )}
+                </NavLink>
+                <NavLink to="/dm">
+                    {({ isActive }) => (
+                        <img src={isActive ? iconDMActive : iconDM} alt="HomeIcon" />
+                    )}
+                </NavLink>
             </footer>
         </>
     );
