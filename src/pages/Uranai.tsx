77359082bc1b3d32d61e4fd0cba1dd910e9c82e1
ShunKUN1182 <2250378@ecc.ac.jp>
2026-07-22 03:29:55 +0900

    feat: uranaiページ作成中

--- src/pages/Uranai.tsx
+++ src/pages/Uranai.tsx
@@ -1,16 +1,53 @@
+import { useState } from "react";
 import "./css/Uranai.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import kaba from "../assets/characters/kaba.png";
+import cardBack from "../assets/background/card_back_bg.png";
 
 function Uranai() {
+    const [isResultVisible, setIsResultVisible] = useState(false);
+
+    if (isResultVisible) {
+        return (
+            <div className="uranai_wrap">
+                <div className="card_wrap">
+                    <div>
+                        <img src={cardBack} alt="" />
+                    </div>
+                    <div>
+                        <img src={cardBack} alt="" />
+                    </div>
+                    <div>
+                        <img src={cardBack} alt="" />
+                    </div>
+                    <div>
+                        <img src={cardBack} alt="" />
+                    </div>
+                    <div>
+                        <img src={cardBack} alt="" />
+                    </div>
+                    <div>
+                        <img src={cardBack} alt="" />
+                    </div>
+                </div>
+                <div className="chara_wrap">
+                    <p>どれか選ぶかばー</p>
+                    <img src={kaba} alt="カバのキャラクター" />
+                </div>
+            </div>
+        );
+    }
+
     return (
-        <>
-            <Header />
-            <main>
-                <h1>フレンドだよ</h1>
-            </main>
-            <Footer />
-        </>
+        <div className="uranai_wrap">
+            <button className="tapBtn" onClick={() => setIsResultVisible(true)}>
+                タップして占う！
+            </button>
+
+            <div className="chara_wrap">
+                <p>なにがでるかば！？</p>
+                <img src={kaba} alt="カバのキャラクター" />
+            </div>
+        </div>
     );
 }
 
