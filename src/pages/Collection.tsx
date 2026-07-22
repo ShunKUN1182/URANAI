f7f57cb6046b9831b48f14660a19eb3598d58ef2
ShunKUN1182 <2250378@ecc.ac.jp>
2026-07-22 10:10:48 +0900

    style: コレクション画面

--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -2,6 +2,10 @@ import "./css/Collection.css";
 import Footer from "../components/Footer";
 import iconLeftArrow from "../assets/icons/icon_arrow_left.png";
 import iconBell from "../assets/icons/icon_bell.png";
+import kaba from "../assets/characters/kaba.png";
+import ghost from "../assets/characters/ghost.png";
+import hitujii from "../assets/characters/hitujii.png";
+
 import { Link } from "react-router-dom";
 
 function Collection() {
@@ -14,8 +18,29 @@ function Collection() {
                 <h1>コレクション</h1>
                 <img src={iconBell} alt="" />
             </div>
-            <main>
-                <h1>コレクションだよ</h1>
+            <main className="collection_main">
+                <div className="complete_wrap">
+                    <h2>コンプリート率</h2>
+                    <span id="bar"></span>
+                </div>
+                <div className="filter_wrap">
+                    <span>すべて</span>
+                    <p>所持済み</p>
+                </div>
+                <div className="collection_wrap">
+                    <div className="collection_card">
+                        <img src={kaba} alt="" />
+                        <h2>カバ太郎</h2>
+                    </div>
+                    <div className="collection_card">
+                        <img src={ghost} alt="" />
+                        <h2>しょんぼ霊</h2>
+                    </div>
+                    <div className="collection_card secret">
+                        <img src={hitujii} alt="" />
+                        <h2>ひつじい</h2>
+                    </div>
+                </div>
             </main>
             <Footer />
         </>
