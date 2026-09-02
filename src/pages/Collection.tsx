38aa552ba2fa2738b7bed58659bb620b08acad04
ShunKUN1182 <2250378@ecc.ac.jp>
2026-09-03 01:05:53 +0900

    feat: キャラ一覧をコレクション画面に表示

--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -5,10 +5,38 @@ import iconBell from "../assets/icons/icon_bell.png";
 import kaba from "../assets/characters/kaba.png";
 import ghost from "../assets/characters/ghost.png";
 import hitujii from "../assets/characters/hitujii.png";
+import { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 
 function Collection() {
+    const [characters, setCharacters] = useState<any[]>([]);
+    const characterImages: { [key: string]: string } = {
+        kaba: kaba,
+        ghost: ghost,
+        hitujii: hitujii,
+    };
+
+    async function getCharacters() {
+        try {
+            const response = await fetch("https://fksm.tonkotsu.jp/uranai/characters.php");
+
+            const data = await response.json();
+
+            console.log(data);
+
+            if (!data.success) {
+                throw new Error(data.message);
+            }
+
+            setCharacters(data.characters);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    useEffect(() => {
+        getCharacters();
+    }, []);
     return (
         <>
             <div className="collection_header">
@@ -28,54 +56,12 @@ function Collection() {
                     <p>所持済み</p>
                 </div>
                 <div className="collection_wrap">
-                    <div className="collection_card">
-                        <img src={kaba} alt="" />
-                        <h2>カバ太郎</h2>
-                    </div>
-                    <div className="collection_card">
-                        <img src={ghost} alt="" />
-                        <h2>しょんぼ霊</h2>
-                    </div>
-                    <div className="collection_card secret">
-                        <img src={hitujii} alt="" />
-                        <h2>ひつじい</h2>
-                    </div>
-                    <div className="collection_card secret">
-                        <img src={hitujii} alt="" />
-                        <h2>ひつじい</h2>
-                    </div>{" "}
-                    <div className="collection_card secret">
-                        <img src={hitujii} alt="" />
-                        <h2>ひつじい</h2>
-                    </div>{" "}
-                    <div className="collection_card secret">
-                        <img src={hitujii} alt="" />
-                        <h2>ひつじい</h2>
-                    </div>{" "}
-                    <div className="collection_card secret">
-                        <img src={hitujii} alt="" />
-                        <h2>ひつじい</h2>
-                    </div>{" "}
-                    <div className="collection_card secret">
-                        <img src={hitujii} alt="" />
-                        <h2>ひつじい</h2>
-                    </div>{" "}
-                    <div className="collection_card secret">
-                        <img src={hitujii} alt="" />
-                        <h2>ひつじい</h2>
-                    </div>{" "}
-                    <div className="collection_card secret">
-                        <img src={hitujii} alt="" />
-                        <h2>ひつじい</h2>
-                    </div>{" "}
-                    <div className="collection_card secret">
-                        <img src={hitujii} alt="" />
-                        <h2>ひつじい</h2>
-                    </div>{" "}
-                    <div className="collection_card secret">
-                        <img src={hitujii} alt="" />
-                        <h2>ひつじい</h2>
-                    </div>
+                    {characters.map((character) => (
+                        <div className="collection_card" key={character.id}>
+                            <img src={characterImages[character.image]} alt="" />
+                            <h2>{character.name}</h2>
+                        </div>
+                    ))}
                 </div>
             </main>
             <Footer />
